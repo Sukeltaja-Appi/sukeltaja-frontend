2eb94ecb605b2fa2f6a9b1a356edf3c15def6bd8
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2019-01-20 14:18:07 +0200

    added simple example tests to hello world mock app

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,3 +1,26 @@
-it('works', () => {
-  expect(1).toBe(1);
-});
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+
+import HomeScreen from '../components/HomeScreen'
+
+describe('<HomeScreen/>', () => {
+  it('renders the home screen', () => {
+    const homeScreen = renderer
+      .create(<HomeScreen/>)
+      .toJSON()
+    expect(homeScreen).toMatchSnapshot()
+  })
+
+  it('the counter starts from 0', () => {
+    const homeScreen = shallow(<HomeScreen/>)
+    expect(homeScreen.state().counter).toEqual(0)
+  })
+
+  it('increases the counter when clicked', () => {
+    const homeScreen = shallow(<HomeScreen/>)
+    const button = homeScreen.find("Button")
+    button.props().onPress()
+    expect(homeScreen.state().counter).toEqual(1)
+  })
+})
