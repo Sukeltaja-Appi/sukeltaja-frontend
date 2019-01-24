1cba175692fd548cb2031a7cb5b7e075b04d0fe2
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2019-01-24 22:54:30 +0200

    stylesLocal out from component

--- components/simple/ListItem.js
+++ components/simple/ListItem.js
@@ -2,33 +2,33 @@ import React from 'react'
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
 import ArrowRight from './ArrowRight'
 
-const ListItem = (props) => {
-  const stylesLocal = StyleSheet.create({
-    item: {
-      borderBottomColor: '#ccc',
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      backgroundColor: 'white'
-    },
-    itemRow: {
-      marginTop: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    title: {
-      fontSize: 17,
-      fontWeight: 'bold',
-      marginLeft: 15,
-      marginTop: 0
-    },
-    desc: {
-      fontSize: 15,
-      marginTop: 5,
-      marginLeft: 15,
-      marginBottom: 5,
-      color: '#555'
-    }
-  })
+const stylesLocal = StyleSheet.create({
+  item: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    backgroundColor: 'white'
+  },
+  itemRow: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  title: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginTop: 0
+  },
+  desc: {
+    fontSize: 15,
+    marginTop: 5,
+    marginLeft: 15,
+    marginBottom: 5,
+    color: '#555'
+  }
+})
 
+const ListItem = (props) => {
   return (
     <View style={stylesLocal.item}>
       <TouchableHighlight underlayColor='#d9d9d9' onPress={props.onPress}>
