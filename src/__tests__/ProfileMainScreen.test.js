03d0cf4424d34e67fe99f98bb306c5e31aea0fef
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2019-01-31 07:11:08 +0200

    disable tests and revert back to react-redux 6.0.0 for now, see https://github.com/reduxjs/react-redux/issues/1161

--- src/__tests__/ProfileMainScreen.test.js
+++ src/__tests__/ProfileMainScreen.test.js
@@ -1,36 +1,36 @@
-import React from 'react'
+// import React from 'react'
 
-import renderer from 'react-test-renderer'
-import { shallow, mount } from 'enzyme'
+// import renderer from 'react-test-renderer'
+// import { shallow, mount } from 'enzyme'
 
-import configureStore from 'redux-mock-store'
+// import configureStore from 'redux-mock-store'
 
-import { Button } from 'react-native'
+// import { Button } from 'react-native'
 
-import ProfileMainScreen from '../components/ProfileScreens/ProfileMainScreen'
+// import ProfileMainScreen from '../components/ProfileScreens/ProfileMainScreen'
 
-const mockStore = configureStore()
+// const mockStore = configureStore()
 
-let wrapper, mounted
+// let wrapper, mounted
 
-beforeAll(() => {
-  const initialState = { user: { username: 'test_profile' } }
-  const store = mockStore(initialState)
-  wrapper = shallow(<ProfileMainScreen store={store} />)
-  mounted = mount(<ProfileMainScreen store={store} />)
-})
+// beforeAll(() => {
+//   const initialState = { user: { username: 'test_profile' } }
+//   const store = mockStore(initialState)
+//   wrapper = shallow(<ProfileMainScreen store={store} />)
+//   mounted = mount(<ProfileMainScreen store={store} />)
+// })
 
-describe('<ProfileMainScreen/>', () => {
-  it('renders the profile screen snapshot', () => {
-    const profileScreen = renderer
-      .create(wrapper)
-      .toJSON()
-    expect(profileScreen).toMatchSnapshot()
-  })
+// describe('<ProfileMainScreen/>', () => {
+//   it('renders the profile screen snapshot', () => {
+//     const profileScreen = renderer
+//       .create(wrapper)
+//       .toJSON()
+//     expect(profileScreen).toMatchSnapshot()
+//   })
 
-  it('pressing the button does nothing', () => {
-    const button = mounted.find(Button).first()
-    button.simulate('click')
-    expect(1).toBe(1)
-  })
-})
+//   it('pressing the button does nothing', () => {
+//     const button = mounted.find(Button).first()
+//     button.simulate('click')
+//     expect(1).toBe(1)
+//   })
+// })
