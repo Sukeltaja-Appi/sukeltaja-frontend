f9efb2cfcb448c5e50fa322ca3f1ded9c89f46b1
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2019-03-08 09:30:34 +0200

    fixed magic number

--- src/utils/coordinates.js
+++ src/utils/coordinates.js
@@ -1,11 +1,12 @@
 const decimalToDMS = (coordinate) => {
   const mul = 60
+  const precision = 4
 
   const degrees = (coordinate) => Math.floor(coordinate)
   const minutes = (coordinate) => Math.floor((coordinate - degrees(coordinate)) * mul)
   const seconds = (coordinate) => ((coordinate - degrees(coordinate)) * mul - minutes(coordinate)) * mul
 
-  const formatted = `${degrees(coordinate)}° ${minutes(coordinate)}' ${seconds(coordinate).toFixed(4)}"`
+  const formatted = `${degrees(coordinate)}° ${minutes(coordinate)}' ${seconds(coordinate).toFixed(precision)}"`
 
   return formatted
 }
