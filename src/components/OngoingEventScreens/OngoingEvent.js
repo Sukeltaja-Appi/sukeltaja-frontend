66732da251fdbcb640e271b68b8de2a5f82dbce2
Veikko Laine <veikko.laine@gmail.com>
2019-03-10 15:42:39 +0200

    merge conflicts solved...

--- src/components/OngoingEventScreens/OngoingEvent.js
+++ src/components/OngoingEventScreens/OngoingEvent.js
@@ -1,67 +1,147 @@
 import React from 'react'
+import { View, FlatList } from 'react-native'
+import { Text, Button, ListItem } from 'react-native-elements'
 import { connect } from 'react-redux'
-import locationService from '../../services/location'
-import { View } from 'react-native'
-import { Text, Button } from 'react-native-elements'
-import { endEvent } from '../../reducers/eventReducer'
-import { startDive } from '../../reducers/diveReducer'
-import styles from '../../styles/global'
+
+import { endEvent, mergeOngoingEvent, leaveOngoingEvent, getOngoingEvent } from '../../reducers/eventReducer'
+import { endDive } from '../../reducers/diveReducer'
 import colors from '../../styles/colors'
+import { usernameOrId, objectToID } from '../../utils/utilityFunctions'
 
 const style = {
   buttonEnd: {
-    ...styles.roundButton,
-    backgroundColor: colors.red
+    backgroundColor: colors.red,
   },
-  buttonDive: {
-    ...styles.roundButton,
-    backgroundColor: colors.green,
+  buttonInvite: {
+    backgroundColor: colors.green
+  },
+  main: {
+    flex: 1,
+    backgroundColor: colors.background,
+    paddingTop: 15,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 10,
+  },
+  buttonDivider: {
+    height: 15
   }
 }
 
-const OngoingEvent = (props) => {
-  const { ongoingEvent, endEvent, navigation } = props
+const OngoingEventScreen = (props) => {
 
-  const navigate = (value) => navigation.navigate(value)
+  const navigate = (value) => props.navigation.navigate(value)
 
-  const endEventButton = () => {
-    endEvent(ongoingEvent)
+  const endDives = async () => {
+    const { endDive, ongoingEvent, ongoingDive } = props
+
+    if(typeof ongoingDive !== 'undefined' && ongoingDive !== null) {
+
+      ongoingDive.enddate = new Date()
+      ongoingDive.event = ongoingEvent.id
+      await endDive(ongoingDive)
+    }
   }
 
-  const diveButton = async () => {
-    let dive = {
-      event: props.ongoingEvent._id,
-      startdate: new Date(),
-      latitude: null,
-      longitude: null
+  const endEventButton = async () => {
+    let { ongoingEvent, selectedTargets } = props
+    const length = selectedTargets.length
+
+    if(length > 0) ongoingEvent.target = selectedTargets[length-1].id
+
+    await endDives()
+    await props.endEvent(ongoingEvent)
+
+    navigate('StartEventScreen')
+  }
+
+  const leaveEventButton = () => {
+    props.leaveOngoingEvent()
+    navigate('StartEventScreen')
+  }
+
+  const endButton = async () => {
+    const { user, ongoingEvent } = props
+
+    if(user.id === objectToID(ongoingEvent.creator)) {
+      endEventButton()
     }
+    leaveEventButton()
+  }
 
-    const location = await locationService.getLocationAsync()
+  const endButtonText = () => {
+    const { ongoingEvent, user } = props
 
-    dive.latitude = location.coords.latitude
-    dive.longitude = location.coords.longitude
+    if(user.id === objectToID(ongoingEvent)) return 'Lopeta'
 
-    await props.startDive(dive)
+    return 'Poistu'
+  }
 
-    navigate('DiveScreen')
+  const pressUser = () => {
+
+  }
+
+  let users = []
+
+  if (typeof props.ongoingEvent !== 'undefined' && props.ongoingEvent !== null ) {
+    const { creator, admins, participants } = props.ongoingEvent
+
+    users = [ creator, ...admins, ...participants ]
   }
 
   return (
-    <View style={styles.centered}>
-      <Text h1>{ongoingEvent.title}</Text>
-      <View style={styles.bottom}>
-        <Button title='Lopeta' onPress={() => endEventButton()} buttonStyle={style.buttonEnd} raised />
+    <View style={style.main}>
+      <Text h3 >Meneillään oleva tapahtuma</Text>
+      <View style={{ height: 20 }} />
+
+      <Text h4>Osallistujat:</Text>
+
+      <View style={style.list}>
+        <FlatList
+          data={users}
+          renderItem={({ item }) => {
+
+            return (
+              <ListItem
+                title={usernameOrId(item)}
+                onPress={() => pressUser(item)}
+                bottomDivider
+              />
+            )}
+          }
+          keyExtractor={item => objectToID(item)}
+        />
+
       </View>
-      <View style={styles.bottom}>
-        <Button title='Sukella' onPress={() => diveButton()} buttonStyle={style.buttonDive} raised />
+
+      <View style={style.bottom}>
+        <Button
+          title='+ Kutsu lisää osallistujia'
+          onPress={() => navigate('InviteScreen')}
+          buttonStyle={style.buttonInvite}
+          raised
+        />
+        <View style={style.buttonDivider}/>
+        <Button title={endButtonText()} onPress={() => endButton()} buttonStyle={style.buttonEnd} raised />
       </View>
     </View>
   )
 }
 
-const mapStateToProps = (state) => ({ ongoingEvent: state.ongoingEvent })
+const mapStateToProps = (state) => ({
+  ongoingEvent: state.ongoingEvent,
+  ongoingDive: state.ongoingDive,
+  selectedTargets: state.selectedTargets,
+  user: state.user
+})
 
 export default connect(
   mapStateToProps,
-  { endEvent, startDive }
-)(OngoingEvent)
+  { endEvent, mergeOngoingEvent, endDive, leaveOngoingEvent, getOngoingEvent }
+)(OngoingEventScreen)
