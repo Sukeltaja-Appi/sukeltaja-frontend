b62c194a80619c4937975e36ce2e9afbd3fc801d
Veikko Laine <veikko.laine@gmail.com>
2019-04-09 22:24:00 +0300

    fixed some errors

--- src/components/ProfileScreens/ResetScreen.js
+++ src/components/ProfileScreens/ResetScreen.js
@@ -3,92 +3,88 @@ import { connect } from 'react-redux'
 import { View } from 'react-native'
 import { Button } from 'react-native-elements'
 import t from 'tcomb-form-native'
-import { reset } from '../../reducers/userReducer'
 import resetService from '../../services/reset'
-
 import { paddingSides } from '../../styles/global'
 
 const { Form } = t.form
 
 const style = {
-    container: {
-        width: '100%',
-        backgroundColor: 'white',
-        padding: paddingSides,
-        paddingBottom: 5
-    },
-    buttonDivider: {
-        height: 20
-    },
-    bottomButton: {
-        position: 'fixed',
-        bottom: 10
-    }
+  container: {
+    width: '100%',
+    backgroundColor: 'white',
+    padding: paddingSides,
+    paddingBottom: 5
+  },
+  buttonDivider: {
+    height: 20
+  },
+  bottomButton: {
+    position: 'fixed',
+    bottom: 10
+  }
 }
 
 const options = {
-    fields: {
-        username: {
-            label: 'Käyttäjätunnus:',
-            error: 'Käyttäjätunnus ei saa olla tyhjä.'
-        }
+  fields: {
+    username: {
+      label: 'Käyttäjätunnus:',
+      error: 'Käyttäjätunnus ei saa olla tyhjä.'
     }
+  }
 }
 
 class ResetScreen extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            username: ''
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: ''
 
-        }
     }
+  }
 
     navigate = (value) => this.props.navigation.navigate(value)
 
     resetPassword = async () => {
-        console.log(this.state.username)
-        console.log('Resetting password for: ' + this.state.username.username)
-    
-        await resetService.reset(this.state.username)
+      console.log(this.state.username)
+      console.log('Resetting password for: ' + this.state.username.username)
 
-        
-        console.log('Email sent')
-        this.navigate('LoginScreen')
+      await resetService.reset(this.state.username)
+
+      console.log('Email sent')
+      this.navigate('LoginScreen')
 
     }
 
     render() {
-        const { username } = this.state
-        const User = t.struct({
-            username: t.String
-        })
-        const reference = 'form'
+      const { username } = this.state
+      const User = t.struct({
+        username: t.String
+      })
+      const reference = 'form'
 
-        return (
-            
-            <View style={style.container}>
-            
-                <Form
-                    ref={reference}
-                    type={User}
-                    options={options}
-                    value={username}
-                    onChange={(username) => this.setState({ username })}
-                />
+      return (
 
-                <Button
-                    onPress={this.resetPassword}
-                    title="Lähetä"
-                />
+        <View style={style.container}>
 
-            </View>
-        )
+          <Form
+            ref={reference}
+            type={User}
+            options={options}
+            value={username}
+            onChange={(username) => this.setState({ username })}
+          />
+
+          <Button
+            onPress={this.resetPassword}
+            title="Lähetä"
+          />
+
+        </View>
+      )
     }
 }
 
-
 export default connect(
-    null,
-    null
+  null,
+  null
 )(ResetScreen)
--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
 import { API_URL } from 'react-native-dotenv'
-<<<<<<< HEAD
-//no se env 
-=======
-//env1
->>>>>>> bc396bd608ddd6c0e8e3b295031ae8c75359aa0f
+//env
 export const apiUrl = API_URL
