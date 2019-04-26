20220ed6213d63d32fb73248d849c30aee6f3dcf
Veikko Laine <veikko.laine@gmail.com>
2019-04-26 16:16:06 +0300

    Event chat now works!

---
+++ src/services/eventMessages.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import axiosRetry from 'axios-retry'
+import { config } from './users'
+import { apiUrl } from '../config'
+
+axiosRetry(axios, {
+  retries: 5,
+  retryDelay: axiosRetry.exponentialDelay
+})
+
+const url = `${apiUrl}/eventMessages`
+
+const create = async (newObject) => {
+  const response = await axios.post(url, newObject, config())
+
+  return response.data
+}
+
+const update = async (id, updatedObject) => {
+  const response = await axios.put(`${url}/${id}`, updatedObject, config())
+
+  return response.data
+}
+
+export default { create, update }
