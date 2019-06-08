a69c96522c0cbe48064490d30cc32541448960c7
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2019-06-08 14:57:03 +0300

    remove the temporary transform fix

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,6 @@ const { defaults } = require('jest-config')
 module.exports = {
   'preset': 'jest-expo',
   'setupFilesAfterEnv': ['<rootDir>/setup-tests.config.js'],
-  'transform': {
-    /*
-     * Stop jest from falling over on its face.
-     * cf. https://github.com/expo/expo/issues/2595#issuecomment-440966998
-     * cf. https://github.com/facebook/react-native/issues/22175#issuecomment-436959462
-     */
-    '\\.js$': '<rootDir>/jest.preprocessor.js',
-  },
   'transformIgnorePatterns': [
     'node_modules/(?!react-native|react-navigation)/'
   ],
