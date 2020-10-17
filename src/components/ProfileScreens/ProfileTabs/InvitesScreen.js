4c0aa64c7866e0fe7b2321ee77913f28ddbfa4d8
Jesse Niininen <findarkside@gmail.com>
2020-10-17 17:34:57 +0300

    Implement new invites screen

--- src/components/ProfileScreens/ProfileTabs/InvitesScreen.js
+++ src/components/ProfileScreens/ProfileTabs/InvitesScreen.js
@@ -1,27 +1,47 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { View, FlatList, Text } from 'react-native'
-import { ListItem, Button } from 'react-native-elements'
+import { View, FlatList, Text, StyleSheet, TouchableHighlight, RefreshControl } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import { MaterialIcons } from '@expo/vector-icons'
 
 import { getMessages } from '../../../reducers/messageReducer'
-import { formatDate } from '../../../utils/dates'
 import styles from '../../../styles/global'
+import colors from '../../../styles/colors'
+import AppText from '../../common/AppText'
+import { joinOngoingEvent } from '../../../reducers/eventReducer'
+import { checkMessage } from '../../../reducers/messageReducer'
 
-const style = {
-  subtitle: {
-    fontStyle: 'italic',
-    fontSize: 14
+const style = StyleSheet.create({
+  acceptButton: {
+    padding: 10,
+    borderRadius: 100,
+    marginBottom: 5,
+    borderColor: '#e8e8e8'
+  },
+  rejectButton: {
+    padding: 10,
+    borderRadius: 100,
+    borderColor: '#e8e8e8',
+    marginRight: 15
   },
   divider: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
     height: 10
+  },
+  flexRowCenter: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row'
   }
-}
+})
 
 class InvitesScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      invites: []
+      invites: [],
+      isFetching: false,
     }
 
     this.loadMessages()
@@ -45,7 +65,9 @@ class InvitesScreen extends React.Component {
   }
 
   loadMessages = async () => {
+    this.setState({ isFetching: true })
     await this.props.getMessages()
+    this.setState({ isFetching: false })
     this.updateInvites()
   }
 
@@ -64,47 +86,88 @@ class InvitesScreen extends React.Component {
     return this.state.invites.sort((a, b) => b.created.localeCompare(a.created))
   }
 
+  async acceptInvite(invite) {
+    await this.props.joinOngoingEvent(invite.data)
+    await this.props.checkMessage(invite, 'accepted')
+    this.updateInvites()
+  }
+
+  async rejectInvite(invite) {
+    console.log('REJECT ', invite)
+    await this.props.checkMessage(invite, 'rejected')
+    this.updateInvites()
+  }
+
   showMessages = () => {
-    if (this.state.invites.length === 0) {
-      return (
-        <View style={styles.centered}>
-          <Text style={styles.h5}>Ei kutsuja.</Text>
-          <Button
-            title="Hae kutsut"
-            onPress={this.loadMessages}
-          />
-        </View>
-      )
-    }
+    const formatDDMM = d => `${d.getDate()}.${d.getMonth() + 1}`
 
     return (
       <View style={styles.noPadding}>
         <FlatList
           data={this.InvitesSortedByDate()}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isFetching}
+              onRefresh={this.loadMessages}
+              colors={['#118BFC']}
+            />
+          }
+
+          ListEmptyComponent={() => (
+            <View style={styles.centered}>
+              <AppText style={styles.h5}>Ei kutsuja</AppText>
+            </View>
+          )
+          }
+
           renderItem={({ item }) => {
-            const { sender, created } = item
+            const { sender, created, data } = item
+            let { title, startdate, enddate } = data
+
+            startdate = new Date(startdate)
+            enddate = new Date(enddate)
 
             return (
               <ListItem
                 onPress={() => this.selectMessage(item)}
                 bottomDivider
+                containerStyle={{ paddingVertical: 0, paddingHorizontal: 16 }}
               >
-                <ListItem.Content>
-                  <ListItem.Title>{sender.username}</ListItem.Title>
-                  <ListItem.Subtitle style={style.subtitle}>{formatDate(created)}</ListItem.Subtitle>
+                <ListItem.Content style={{ ...style.flexRowCenter }}>
+                  <View style={{ paddingVertical: 16, flexShrink: 1 }}>
+                    <AppText style={{ color: colors.primaryText, fontSize: 18, flex: 1, flexWrap: 'wrap' }} >
+                      {title}
+                    </AppText>
+                    <View style={{ ...style.flexRowCenter }}>
+                      <MaterialIcons name='person' size={18} color={'#606060'} />
+                      <Text style={{ paddingLeft: 5 }}>{sender.username}</Text>
+                    </View>
+                    <View style={{ ...style.flexRowCenter }}>
+                      <MaterialIcons name='date-range' size={18} color={'#606060'} />
+                      <Text style={{ paddingLeft: 5 }}>
+                        {`${formatDDMM(startdate)}. - ${formatDDMM(enddate)}.${enddate.getFullYear()}`}
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={{ flexGrow: 1 }} />
+                  <TouchableHighlight style={style.rejectButton}
+                    onPress={() => this.rejectInvite(item)}
+                    underlayColor='#d4d4d4'
+                  >
+                    <MaterialIcons name='clear' size={40} color={'#999'} />
+                  </TouchableHighlight>
+                  <TouchableHighlight style={style.acceptButton}
+                    onPress={() => this.acceptInvite(item)}
+                    underlayColor='#d4d4d4'
+                  >
+                    <MaterialIcons name='check' size={40} color={'#68FF77'} />
+                  </TouchableHighlight>
                 </ListItem.Content>
-                <ListItem.Chevron />
               </ListItem>
             )
-          }
-          }
+          }}
           keyExtractor={item => item._id}
         />
-        <Button
-          title="Hae kutsut"
-          onPress={this.loadMessages}
-        />
-        <View style={style.divider} />
       </View>
     )
   }
@@ -122,5 +185,5 @@ const mapStateToProps = (state) => ({
 
 export default connect(
   mapStateToProps,
-  { getMessages }
+  { getMessages, joinOngoingEvent, checkMessage }
 )(InvitesScreen)
