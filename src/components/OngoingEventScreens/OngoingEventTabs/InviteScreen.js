b0714d14de76eb12d8e424e5345329dbcbe936b6
Joonas Alanenpää <joonas.alanenpaa@gmail.com>
2020-10-29 03:21:59 +0200

    invite screen front ui updated

--- src/components/OngoingEventScreens/OngoingEventTabs/InviteScreen.js
+++ src/components/OngoingEventScreens/OngoingEventTabs/InviteScreen.js
@@ -1,40 +1,43 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import { View, FlatList, Text } from 'react-native'
-import { CheckBox, Button, SearchBar } from 'react-native-elements'
+import { View, FlatList, Text } from "react-native";
+import { CheckBox, Button, SearchBar } from "react-native-elements";
 
-import { loadAllUsers } from '../../../reducers/userReducer'
-import { sendMessage } from '../../../reducers/messageReducer'
-import { getOngoingEvent } from '../../../reducers/eventReducer'
+import { loadAllUsers } from "../../../reducers/userReducer";
+import { sendMessage } from "../../../reducers/messageReducer";
+import { getOngoingEvent } from "../../../reducers/eventReducer";
 
-import styles from '../../../styles/global'
-import colors from '../../../styles/colors'
+import styles, { paddingSides } from "../../../styles/global";
+import colors from "../../../styles/colors";
+import AppText from "../../common/AppText";
 
 const style = {
   divider: {
-    height: 10
+    height: 10,
   },
   buttonGreen: {
     backgroundColor: colors.green,
+    fontFamily: "nunito-bold",
   },
   top: {
-    flex: 2
+    flex: 0,
   },
   middle: {
-    flex: 8
+    flex: 4,
   },
   bottom: {
-    flex: 5
+    flex: 2,
+    padding: paddingSides,
   },
   searchContainer: {
-    backgroundColor: 'transparent',
-    justifyContent: 'flex-start',
-    borderBottomColor: 'transparent',
-    borderTopColor: 'transparent'
+    backgroundColor: "transparent",
+    justifyContent: "flex-start",
+    borderBottomColor: "transparent",
+    borderTopColor: "transparent",
   },
   searchInputContainer: {
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
     height: 50,
     borderColor: colors.lightgray,
     borderBottomColor: colors.gray,
@@ -42,104 +45,117 @@ const style = {
     borderBottomWidth: 1,
   },
   searchInput: {
-    backgroundColor: 'rgba(255, 255, 255, 0.05)',
-    color: 'black'
-  }
-}
+    backgroundColor: "rgba(255, 255, 255, 0.05)",
+    color: "black",
+    fontFamily: "nunito-bold",
+    fontWeight: "normal",
+  },
+};
 
 class InviteScreen extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       displayedUsers: [],
       selectedUsers: [],
-      query: '',
-      listMessage: 'Ei käyttäjiä'
-    }
-    this.showList = this.showList.bind(this)
+      query: "",
+      listMessage: "Ei käyttäjiä",
+    };
+    this.showList = this.showList.bind(this);
   }
 
   componentDidMount() {
-    if(this.props.users.length === 0) this.loadUsers()
+    if (this.props.users.length === 0) this.loadUsers();
   }
 
-  navigate = (value) => this.props.navigation.navigate(value)
+  navigate = (value) => this.props.navigation.navigate(value);
 
   loadUsers = async () => {
-    await this.props.loadAllUsers()
-  }
+    await this.props.loadAllUsers();
+  };
 
-  userIsCreator = (user) => user._id === this.props.ongoingEvent.creator._id
-  userIsAdmin = (user) => this.props.ongoingEvent.admins.map(a => a._id).includes(user._id)
-  userIsParticipant = (user) => this.props.ongoingEvent.participants.map(p => p._id).includes(user._id)
-  userIsPending = (user) => this.props.ongoingEvent.pending.map(p => p.user._id).includes(user._id)
+  userIsCreator = (user) => user._id === this.props.ongoingEvent.creator._id;
+  userIsAdmin = (user) =>
+    this.props.ongoingEvent.admins.map((a) => a._id).includes(user._id);
+  userIsParticipant = (user) =>
+    this.props.ongoingEvent.participants.map((p) => p._id).includes(user._id);
+  userIsPending = (user) =>
+    this.props.ongoingEvent.pending.map((p) => p.user._id).includes(user._id);
 
   sendInvite = async (type) => {
-    const { userIsCreator, userIsAdmin } = this
-    const { selectedUsers } = this.state
-    const { sendMessage, user, ongoingEvent, getOngoingEvent } = this.props
+    const { userIsCreator, userIsAdmin } = this;
+    const { selectedUsers } = this.state;
+    const { sendMessage, user, ongoingEvent, getOngoingEvent } = this.props;
 
-    if (!selectedUsers || !userIsCreator(user) && !userIsAdmin(user)) return
+    if (!selectedUsers || (!userIsCreator(user) && !userIsAdmin(user))) return;
 
-    const receivers = selectedUsers.map(receiver => receiver._id)
+    const receivers = selectedUsers.map((receiver) => receiver._id);
 
-    await sendMessage(
-      type,
-      ongoingEvent,
-      user._id,
-      receivers
-    )
+    await sendMessage(type, ongoingEvent, user._id, receivers);
 
-    await getOngoingEvent(ongoingEvent)
-    this.setState({ selectedUsers: [], displayedUsers: [], listMessage: 'Kutsu lähetetty!' })
-    this.loadUsers()
-  }
+    await getOngoingEvent(ongoingEvent);
+    this.setState({
+      selectedUsers: [],
+      displayedUsers: [],
+      listMessage: "Kutsu lähetetty!",
+    });
+    this.loadUsers();
+  };
 
-  inviteAdmins = () => this.sendInvite('invitation_admin')
-  inviteParticipants = () => this.sendInvite('invitation_participant')
+  inviteAdmins = () => this.sendInvite("invitation_admin");
+  inviteParticipants = () => this.sendInvite("invitation_participant");
 
   inviteAdminButtonDisabled = () => {
-    const { selectedUsers } = this.state
+    const { selectedUsers } = this.state;
 
-    return selectedUsers.length === 0 || selectedUsers.some(u => this.userIsAdmin(u))
-  }
+    return (
+      selectedUsers.length === 0 ||
+      selectedUsers.some((u) => this.userIsAdmin(u))
+    );
+  };
   inviteParticipantButtonDisabled = () => {
-    const { selectedUsers } = this.state
+    const { selectedUsers } = this.state;
 
-    return selectedUsers.length === 0 || selectedUsers.some(u => this.userIsParticipant(u))
-  }
+    return (
+      selectedUsers.length === 0 ||
+      selectedUsers.some((u) => this.userIsParticipant(u))
+    );
+  };
 
   backButton = async () => {
-    const { ongoingEvent, getOngoingEvent } = this.props
+    const { ongoingEvent, getOngoingEvent } = this.props;
 
-    await getOngoingEvent(ongoingEvent)
-    this.navigate('EventScreen')
-  }
+    await getOngoingEvent(ongoingEvent);
+    this.navigate("EventScreen");
+  };
 
   toggleUserSelection = (user) => {
-    const { selectedUsers } = this.state
+    const { selectedUsers } = this.state;
 
     if (!selectedUsers.includes(user)) {
-      this.setState({ selectedUsers: [...selectedUsers, user] })
+      this.setState({ selectedUsers: [...selectedUsers, user] });
     } else {
-      this.setState({ selectedUsers: selectedUsers.filter(u => u._id !== user._id) })
+      this.setState({
+        selectedUsers: selectedUsers.filter((u) => u._id !== user._id),
+      });
     }
-  }
+  };
 
   //Sets the users checkbox color.
   // Blue if user has a pening invite.
   // Green if user is already a participant and is not pending.
   setUserColor = (user) => {
-    const { userIsPending, userIsParticipant } = this
+    const { userIsPending, userIsParticipant } = this;
 
-    if (userIsPending(user)) return { backgroundColor: colors.lightBlue }
-    if (userIsParticipant(user)) return { backgroundColor: colors.green }
+    if (userIsPending(user))
+      return { backgroundColor: colors.lightBlue, color: "white" };
+    if (userIsParticipant(user)) return { backgroundColor: colors.green };
 
-    return {}
-  }
+    return {};
+  };
 
   renderListItem = (user) => {
-    const { selectedUsers } = this.state
+    const { selectedUsers } = this.state;
 
     return (
       <CheckBox
@@ -147,109 +163,129 @@ class InviteScreen extends React.Component {
         onPress={() => this.toggleUserSelection(user)}
         checked={selectedUsers.includes(user)}
         containerStyle={this.setUserColor(user)}
+        textStyle={{
+          fontFamily: 'nunito-bold',
+          fontWeight: 'normal',
+          color: this.userIsPending(user) || this.userIsParticipant(user) ? '#fff' : '#000',
+        }}
       />
-    )
-  }
+    );
+  };
 
   showList = (invitableUsers) => {
     if (invitableUsers.length === 0) {
       return (
         <View style={styles.centered}>
-          <Text style={styles.h5}>{this.state.listMessage}</Text>
+          <AppText style={styles.h5}>{this.state.listMessage}</AppText>
         </View>
-      )
+      );
     } else {
       return (
         <FlatList
           data={invitableUsers}
-          renderItem={({ item }) => this.renderListItem(item) }
-          keyExtractor={item => item._id}
+          renderItem={({ item }) => this.renderListItem(item)}
+          keyExtractor={(item) => item._id}
           extraData={this.state}
         />
-      )
+      );
     }
-  }
+  };
 
   search = (query) => {
-    let { selectedUsers } = this.state
+    let { selectedUsers } = this.state;
 
     this.setState({ query }, () => {
-      if (!query || query === '') this.setState({ displayedUsers: selectedUsers, listMessage: 'Ei käyttäjiä!' })
+      if (!query || query === "")
+        this.setState({
+          displayedUsers: selectedUsers,
+          listMessage: "Ei käyttäjiä!",
+        });
       else {
-        query = query.toLowerCase()
-        const { users } = this.props
-        const { userIsAdmin, userIsCreator } = this
+        query = query.toLowerCase();
+        const { users } = this.props;
+        const { userIsAdmin, userIsCreator } = this;
 
-        const invitableUsers = users.filter(u => !userIsAdmin(u) && !userIsCreator(u))
+        const invitableUsers = users.filter(
+          (u) => !userIsAdmin(u) && !userIsCreator(u)
+        );
 
-        let displayedUsers = invitableUsers.filter(u => u.username.toLowerCase().startsWith(query))
+        let displayedUsers = invitableUsers.filter((u) =>
+          u.username.toLowerCase().startsWith(query)
+        );
 
-        displayedUsers = displayedUsers.filter(u => !selectedUsers.includes(u))
+        displayedUsers = displayedUsers.filter(
+          (u) => !selectedUsers.includes(u)
+        );
 
-        this.setState({ displayedUsers: [ ...selectedUsers, ...displayedUsers ] })
+        this.setState({
+          displayedUsers: [...selectedUsers, ...displayedUsers],
+        });
       }
-    })
-  }
+    });
+  };
 
   render() {
-    const { query, displayedUsers } = this.state
+    const { query, displayedUsers } = this.state;
 
     return (
       <View style={styles.noPadding}>
-
         <View style={style.top}>
           <SearchBar
-            placeholder='Etsi käyttäjiä'
+            placeholder="Etsi käyttäjiä"
             containerStyle={style.searchContainer}
             inputContainerStyle={style.searchInputContainer}
             inputStyle={style.searchInput}
-            lightTheme
-            clearIcon={{ name: 'x', type: 'feather', size: 28 }}
-            onChangeText={(query, invitableUsers) => this.search(query, invitableUsers)}
+            clearIcon={{ name: "x", type: "feather", size: 28 }}
+            onChangeText={(query, invitableUsers) =>
+              this.search(query, invitableUsers)
+            }
             value={query}
           />
         </View>
 
-        <View style={style.middle}>
-          {this.showList(displayedUsers)}
-        </View>
+        <View style={style.middle}>{this.showList(displayedUsers)}</View>
 
         <View style={style.bottom}>
           <Button
-            title="+ Kutsu ylläpitäjäksi"
-            onPress={this.inviteAdmins}
-            buttonStyle={style.buttonGreen}
-            raised
-            disabled={this.inviteAdminButtonDisabled()}
-          />
-          <View style={style.divider}/>
-          <Button
-            title="+ Kutsu osallistujaksi"
+            title="Kutsu osallistujaksi"
             onPress={this.inviteParticipants}
             buttonStyle={style.buttonGreen}
+            titleStyle={{ fontFamily: "nunito-bold" }}
             disabled={this.inviteParticipantButtonDisabled()}
             raised
           />
-          <View style={style.divider}/>
+          <View style={style.divider} />
           <Button
-            title="<-- Takaisin"
+            title="Kutsu ylläpitäjäksi"
+            onPress={this.inviteAdmins}
+            buttonStyle={style.buttonGreen}
+            titleStyle={{ fontFamily: "nunito-bold" }}
+            raised
+            disabled={this.inviteAdminButtonDisabled()}
+          />
+          <View style={style.divider} />
+          <View style={style.divider} />
+          <View style={style.divider} />
+          <Button
+            title="Takaisin"
             onPress={this.backButton}
+            titleStyle={{ fontFamily: "nunito-bold" }}
             raised
           />
         </View>
-
       </View>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => ({
   ongoingEvent: state.ongoingEvent,
   users: state.users,
-  user: state.user
-})
+  user: state.user,
+});
 
-export default connect(
-  mapStateToProps,
-  { sendMessage, loadAllUsers, getOngoingEvent }
-)(InviteScreen)
+export default connect(mapStateToProps, {
+  sendMessage,
+  loadAllUsers,
+  getOngoingEvent,
+})(InviteScreen);
