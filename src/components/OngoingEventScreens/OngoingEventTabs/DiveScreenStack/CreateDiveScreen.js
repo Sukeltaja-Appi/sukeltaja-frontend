8d5bfce72f9c6a866ff1f96850e68413e9d25d9b
Joonas Alanenpää <joonas.alanenpaa@gmail.com>
2020-11-27 01:57:12 +0200

    adding dive updated to new front

--- src/components/OngoingEventScreens/OngoingEventTabs/DiveScreenStack/CreateDiveScreen.js
+++ src/components/OngoingEventScreens/OngoingEventTabs/DiveScreenStack/CreateDiveScreen.js
@@ -1,141 +1,252 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { View, ScrollView } from 'react-native'
 import { Button } from 'react-native-elements'
-import DiveForm from '../../../common/DiveForm'
-
 import locationService from '../../../../services/location'
 import { createDive } from '../../../../reducers/diveReducer'
-import { now, inTenMinutes } from '../../../../utils/dates'
-
-import colors from '../../../../styles/colors'
-import styles from '../../../../styles/global'
+import { now, inOneHour, formatDate, inTenMinutes } from '../../../../utils/dates'
+import _ from 'lodash'
 import { paddingSides } from '../../../../styles/global'
+import t from 'tcomb-form-native'
+import RNDateTimePicker from '@react-native-community/datetimepicker'
+import CommonButton from '../../../common/CommonButton'
+import AppText from '../../../common/AppText'
+import { useIsFocused } from '@react-navigation/native'
+import colors from '../../../../styles/colors'
+
+const { Form } = t.form
 
 const style = {
-  divider: {
-    height: 10
+  container: {
+    width: '100%',
+    padding: paddingSides,
+    paddingBottom: 50,
+  },
+  dateButton: {
+    width: '100%',
+    paddingVertical: 10,
+    marginTop: 5,
+    marginBottom: 20,
+    backgroundColor: 'rgba(52, 52, 52, 0.5)',
+    borderRadius: 10,
   },
   buttonContainer: {
-    width: '100%',
-    backgroundColor: 'white',
-    padding: paddingSides,
-    paddingBottom: 50
-  }
+    paddingBottom: 40,
+    marginTop: 40,
+    paddingVertical: 50,
+  },
+  button: {
+    paddingVertical: 10,
+    marginBottom: 20,
+    borderRadius: 10,
+    backgroundColor: colors.primary,
+  },
+  buttonDivider: {
+    height: 20,
+  },
 }
 
-class CreateDiveScreen extends React.Component {
-  constructor(props) {
-    super(props)
-    this.ref = React.createRef()
+const CreateDiveScreen = (props) => {
+  const [startDate, setStartDate] = useState(now())
+  const [endDate, setEndDate] = useState(inTenMinutes())
+  const [dive, setDive] = useState({
+    user: '',
+    longitude: '0.1',
+    latitude: '0.1'
+  })
 
-    let longitude = 0.1
-    let latitude = 0.1
-    const { target } = this.props.ongoingEvent
-
-    if (target) {
-      latitude = target.latitude
-      longitude = target.longitude
-    }
-
-    this.state = {
-      dive: {
-        user: '',
-        startdate: now(),
-        enddate: inTenMinutes(),
-        latitude,
-        longitude
-      }
-    }
-  }
-
-  getLocation = async() => {
+  const getLocation = async() => {
     try {
       const location = await locationService.getLocationAsync()
 
       const latitude = location.coords.latitude
       const longitude = location.coords.longitude
 
-      let dive = this.state.dive
-
       dive.latitude = latitude
       dive.longitude = longitude
-
-      this.setState({ dive })
+      setDive(null)
+      setDive( dive )
     } catch(err) { console.log('Geolocation unavailable.') }
   }
 
   // Any user in the event is allowed to create Dives for themselves
   // To create dives for some other event user, the user must be event admin.
-  createButton = async () => {
-    const validated = this.ref.current.getValue()
-    const { dive } = this.state
+  const createButton = async () => {
 
-    if (validated) {
-      const { user, ongoingEvent, createDive } = this.props
-      let { creator, admins, participants } = ongoingEvent
+    const { user, ongoingEvent, createDive } = props
+    let { creator, admins, participants } = ongoingEvent
 
-      admins = [ creator, ...admins]
-      const allParticipants = [ creator, ...admins, ...participants ]
-      let allowed = false
+    admins = [ creator, ...admins]
+    const allParticipants = [ creator, ...admins, ...participants ]
+    let allowed = false
 
-      if (user.username === dive.user) {
-        dive.user = user._id
-        allowed = true
+    if (user.username === dive.user) {
+      dive.user = user._id
+      allowed = true
 
-      } else if (admins.map(a => a._id).includes(user._id)) {
+    } else if (admins.map(a => a._id).includes(user._id)) {
 
-        for (let i=0; i < allParticipants.length; i++) {
+      for (let i=0; i < allParticipants.length; i++) {
 
-          if(allParticipants[i].username === dive.user) {
+        if(allParticipants[i].username === dive.user) {
 
-            dive.user = allParticipants[i]._id
-            allowed = true
-            break
-          }
+          dive.user = allParticipants[i]._id
+          allowed = true
+          break
         }
       }
+    }
 
-      if(allowed) {
-        dive.event = ongoingEvent._id
-        await createDive(dive, user._id)
-        this.props.navigation.replace('DiveListScreen')
-      }
+    dive.user = user._id
+
+    const diving = {
+      ...dive,
+      startdate: startDate,
+      event: ongoingEvent._id,
+      enddate: endDate,
+    }
+
+    if(allowed) {
+      console.log(diving)
+      console.log(user._id)
+      await createDive(diving, user._id)
+      props.navigation.replace('DiveListScreen')
     }
   }
 
-  navigate = (value) => this.props.navigation.navigate(value)
-
-  render() {
-    const { dive } = this.state
-
-    return (
-      <View style={styles.noPadding}>
-        <ScrollView keyboardShouldPersistTaps='handled'>
-          <DiveForm
-            ref={this.ref}
-            dive={dive}
-            onFormChange={(dive) => this.setState({ dive })}
-            onButtonPress={this.createButton}
-            buttonStyle={{ backgroundColor: colors.success }}
-            buttonTitle="Lisää sukellus"
+  return (
+    <View>
+      <ScrollView keyboardShouldPersistTaps="handled">
+        <View style={style.container}>
+          <Form
+            type={Dive}
+            options={userOptions}
+            value={dive}
+            onChange={(dive) => setDive(dive)}
           />
-          <View style={style.buttonContainer}>
-            <Button
-              title='Hae nykyinen sijaintini'
-              onPress={this.getLocation}
-              raised
-            />
-            <View style={style.divider}/>
-            <Button
-              title='<-- Takaisin'
-              onPress={() => this.navigate('DiveListScreen')}
-              raised
+          <Button
+            buttonStyle={style.button}
+            title='Hae sijainti'
+            onPress={getLocation}
+          />
+          <AppText style={{
+            color: 'black',
+            fontSize: 16,
+            marginBottom: 5,
+            marginLeft: 10
+          }}>Alkamisaika:</AppText>
+          <DateTimePickerButton
+            date={startDate}
+            setDate={setStartDate}
+            text=""
+          />
+          <AppText style={{
+            color: 'black',
+            fontSize: 16,
+            marginBottom: 5,
+            marginLeft: 10
+          }}>Loppumisaika:</AppText>
+          <DateTimePickerButton
+            date={endDate}
+            setDate={setEndDate}
+            text=""
+          />
+          <View syle={style.buttonContainer}>
+            <View style={style.buttonDivider} />
+            <CommonButton
+              title="Lisää sukellus"
+              onPress={createButton}
+              containerStyle={style.submitButton}
             />
           </View>
-        </ScrollView>
-      </View>
-    )
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
+
+const Dive = t.struct({
+  user: t.String,
+  longitude: t.Number,
+  latitude: t.Number
+})
+
+const DateTimePickerButton = (props) => {
+  const { date, setDate, text } = props
+  const [isShowingDatePicker, showDatePicker] = useState(false)
+  const [isShowingTimePicker, showTimePicker] = useState(false)
+  const [localDate, setLocalDate] = useState(date)
+
+  const onDateChange = (event, newDate) => {
+    showDatePicker(false)
+    if (newDate !== undefined) {
+      setDate(newDate)
+      setLocalDate(newDate)
+    }
+    showTimePicker(true)
+  }
+
+  const onTimeChange = (event, newDate) => {
+    showDatePicker(false)
+    showTimePicker(false)
+    if (newDate !== undefined) {
+      setLocalDate(localDate.setMinutes(newDate.getMinutes()))
+      setLocalDate(localDate.setHours(newDate.getHours()))
+      setDate(localDate)
+      setLocalDate(localDate)
+    }
+  }
+
+  return (
+    <View style={style.noPadding}>
+      {isShowingDatePicker && (
+        <RNDateTimePicker mode="date" value={date} onChange={onDateChange} />
+      )}
+      {isShowingTimePicker && (
+        <RNDateTimePicker mode="time" value={date} onChange={onTimeChange} />
+      )}
+      <Button
+        buttonStyle={style.button}
+        title={text + ' ' + formatDate(localDate)}
+        onPress={() => {
+          showDatePicker(true)
+          showTimePicker(false)
+        }}
+      />
+    </View>
+  )
+}
+
+const stylesheet = _.cloneDeep(t.form.Form.stylesheet)
+
+stylesheet.textbox.normal.backgroundColor = 'white'
+stylesheet.controlLabel.normal.color = 'black'
+stylesheet.controlLabel.normal.marginLeft = 15
+stylesheet.textbox.normal.borderRadius = 15
+stylesheet.controlLabel.normal.fontFamily = 'nunito-bold'
+stylesheet.controlLabel.error.fontFamily = 'nunito-bold'
+stylesheet.textbox.error.backgroundColor = 'white'
+stylesheet.controlLabel.error.color = 'white'
+stylesheet.controlLabel.error.marginLeft = 15
+stylesheet.textbox.error.borderRadius = 20
+
+const userOptions = {
+  fields: {
+    user: {
+      label: 'Sukeltajan nimi',
+      error: 'Sukeltajan nimi ei saa olla tyhjä.',
+      stylesheet: stylesheet,
+    },
+    latitude: {
+      label: 'Latitude',
+      error: 'Sukeltajan nimi ei saa olla tyhjä.',
+      stylesheet: stylesheet,
+    },
+    longitude: {
+      label: 'Longitude',
+      error: 'Sukeltajan nimi ei saa olla tyhjä.',
+      stylesheet: stylesheet,
+    }
   }
 }
 
